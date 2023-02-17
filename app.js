7fe5e87d2e4ce5952c956bcab21b683e6536216d
jcasare <asarejerry16@gmail.com>
2023-02-17 13:21:32 +0000

    security middlewares upload

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = 3000 || process.env.PORT;
-
+const cors = require("cors");
+const xss = require("xss-clean");
+app.set("trust-proxy", 1);
+app.use(cors());
+app.use(xss());
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
